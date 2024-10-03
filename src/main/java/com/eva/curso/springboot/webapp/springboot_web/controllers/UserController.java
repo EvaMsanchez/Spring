e7f91d78058aed04c7eadd3bf50f4f627c4814e8
EvaMsanchez <eva_crm20@hotmail.com>
2024-10-03 13:11:39 +0200

    Otra forma de pasar datos

--- src/main/java/com/eva/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/eva/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,7 +1,5 @@
 package com.eva.curso.springboot.webapp.springboot_web.controllers;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,11 +12,11 @@ public class UserController
     // Método
     // Petición a través de una ruta navegador
     @GetMapping("/details")
-    public String details(Map<String, Object> model)
+    public String details(Model model)
     {
-        model.put("title", "Hola Mundo Spring Boot");
-        model.put("name", "Eva");
-        model.put("lastname", "Sánchez");        
+        model.addAttribute("title", "Hola Mundo Spring Boot");
+        model.addAttribute("name", "Eva");
+        model.addAttribute("lastname", "Sánchez");        
         return "details"; // vista
     }
 }
