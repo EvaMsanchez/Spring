354a0213048dec5c84f6d1711b8630612b50cdf1
EvaMsanchez <eva_crm20@hotmail.com>
2024-10-03 20:04:03 +0200

    Lista de objetos

--- src/main/java/com/eva/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/eva/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -2,6 +2,9 @@ package com.eva.curso.springboot.webapp.springboot_web.controllers;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +31,22 @@ public class UserRestController
         userDto.setTitle("Hola Mundo Spring Boot");
         
         return userDto; // devuelve un objeto JSON
+    }
+
+
+    @GetMapping("/list")
+    public List<User> list()
+    {
+        User user = new User("Eva", "Sánchez");
+        User user2 = new User("Pepe", "Doe");
+        User user3 = new User("John", "Doe");
+
+        List<User> users = Arrays.asList(user, user2, user3); // forma más corta
+        //List<User> users = new ArrayList<>();
+        //users.add(user);
+        //users.add(user2);
+        //users.add(user3);
+        return users; // muestra un array de objetos
     }
 
     
