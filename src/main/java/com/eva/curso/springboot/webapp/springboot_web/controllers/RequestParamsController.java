c46489400e2000ffc8acc7b35c6c0520081c51bb
EvaMsanchez <eva_crm20@hotmail.com>
2024-10-11 09:54:10 +0200

    Anotación @PathVariable - para obtener parámetros de la ruta

--- src/main/java/com/eva/curso/springboot/webapp/springboot_web/controllers/RequestParamsController.java
+++ src/main/java/com/eva/curso/springboot/webapp/springboot_web/controllers/RequestParamsController.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-
-
 @RestController
 @RequestMapping("/api/params")
 public class RequestParamsController 
